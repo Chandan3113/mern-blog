aa752cb2c8fb815ecc582b7631c88b6490335e4e
Chandan3113 <21ucs049@lnmiit.ac.in>
2024-06-18 23:18:25 +0530

    Database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
+const { error } = require('console');
 const express=require('express')
+const mongoose=require('mongoose')
+require("dotenv").config()
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    console.log('mongo db is connected')
+}).catch(error=>{
+    console.log(error)
+})
 const app=express();
 
 app.listen(3000,()=>{
