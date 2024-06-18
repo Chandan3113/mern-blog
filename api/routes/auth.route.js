a06100e38d7e8560c390151745f3eb220228c225
Chandan3113 <21ucs049@lnmiit.ac.in>
2024-06-19 02:57:52 +0530

    create signin api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
 const router=express.Router()
-import {signup} from '../controllers/auth.contoller.js'
+import {signup,signin} from '../controllers/auth.contoller.js'
 
 router.post('/signup',signup);
+router.post('/signin',signin);
 
 export default router;
