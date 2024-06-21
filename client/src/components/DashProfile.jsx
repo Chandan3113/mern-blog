b1c1efbf1ee2298d84a237d5a28646a8ecffb565
Chandan3113 <21ucs049@lnmiit.ac.in>
2024-06-22 02:48:24 +0530

    create post ui complete

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -10,9 +10,10 @@ import 'react-circular-progressbar/dist/styles.css'
 import { updateFailure,updateStart,updateSucess,deletUserStart,deleteUserFailure,deleteUserSuccess,signoutSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 import {HiOutlineExclamationCircle} from 'react-icons/hi'
+import {Link} from 'react-router-dom'
 
 const DashProfile = () => {
-    const { currentUser,error } = useSelector(state => state.user)
+    const { currentUser,error,loading } = useSelector(state => state.user)
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -169,10 +170,18 @@ const DashProfile = () => {
                 <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
                 <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
 
-                <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-                    Update
+                <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+                    {loading ? 'Loading...' : 'Update'}
                 </Button>
-
+                {
+                    currentUser.isAdmin && (
+                        <Link to={'/create-post'}>
+                        <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>
+                            Create a post
+                        </Button>
+                        </Link>
+                    )
+                }
             </form>
             <div className='text-red-500 flex justify-between mt-5'>
                 <span className='cursor-pointer' onClick={()=>setShowModal(true)}>Delete Account</span>
